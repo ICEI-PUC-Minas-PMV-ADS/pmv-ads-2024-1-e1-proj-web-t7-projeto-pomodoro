2ca9c748ffee93996b3de3dddf684bf3c6035a63
Pablopas1 <162591442+Pablopas1@users.noreply.github.com>
2024-05-07 18:09:14 -0300

    update home.html; home.js; e style.css

---
+++ codigo-fonte/Home/home.js
@@ -0,0 +1,4 @@
+const circularProgressBar = document.querySelector('#barraProgresso');
+const circularProgressBarNumber = document.querySelector('#barraProgresso .progress-value');
+const buttonTypePomodoro = document.querySelector('#btn-pomodoro');
+const buttonTypeShortBreak = document.querySelector('#pausa');

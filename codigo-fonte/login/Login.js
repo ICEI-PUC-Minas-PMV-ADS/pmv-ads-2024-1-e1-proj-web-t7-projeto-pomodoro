3b734a93d73cc506cee2c4883e7908d1a6a53e8e
Pablopas1 <162591442+Pablopas1@users.noreply.github.com>
2024-05-08 21:25:41 -0300

    Update nas telas login e cadastro

--- codigo-fonte/login/Login.js
+++ codigo-fonte/login/Login.js
@@ -1,11 +1,23 @@
-let btn = document.querySelector('.fa-eye')
+let btn = document.querySelector('.fa-eye');
 
-btn.addEventListener('click', ()=>{
-    let inputSenha = document.querySelector('#senha')
+btn.addEventListener('click', () => {
+    let inputSenha = document.querySelector('#senha');
 
-    if(inputSenha.getAttribute('type') == 'password'){
-        inputSenha.getAttribute('type', 'text')
+    if (inputSenha.getAttribute('type') === 'password') {
+        inputSenha.setAttribute('type', 'text');
     } else {
-        inputSenha.setAttribute('type', 'password')
+        inputSenha.setAttribute('type', 'password');
     }
-})
+});
+
+let btnConfirmeSenha = document.querySelector('#verConfirmeSenha');
+
+btnConfirmeSenha.addEventListener('click', () => {
+    let inputConfirmeSenha = document.querySelector('#confirmeSenha');
+
+    if (inputConfirmeSenha.getAttribute('type') === 'password') {
+        inputConfirmeSenha.setAttribute('type', 'text');
+    } else {
+        inputConfirmeSenha.setAttribute('type', 'password');
+    }
+});

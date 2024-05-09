84307d68d05cf810427238c0d3fa8f5f09ed885b
Pablopas1 <162591442+Pablopas1@users.noreply.github.com>
2024-05-09 01:46:07 -0300

    Update Add LocalStorage

--- codigo-fonte/login/cadastro.js
+++ codigo-fonte/login/cadastro.js
@@ -3,16 +3,30 @@ let btnConfirmeSenha = document.querySelector('#verConfirmeSenha');
 
 //Verificações
 let nome = document.querySelector('#nomeusuario');
+let validnome = false;
+
 let email = document.querySelector('#email');
+let validemail = false;
+
 let telefone = document.querySelector('#telefone');
+let validtelefone = false;
+
 let senha = document.querySelector('#senha');
+let validsenha = false;
+
 let confirmSenha = document.querySelector('#confirmSenha');
+let validconfirmSenha = false;
+
+let msgError = document.querySelector('#msgError');
+let msgSuccess = document.querySelector('#msgSuccess');
 
 nome.addEventListener('keyup', () => {
     if (nome.value.length <= 4) {
         nome.setAttribute('style', 'color: red');
+        validnome = false;
     } else {
         nome.setAttribute('style', 'color: #58C200');
+        validnome = true;
     }
 });
 
@@ -22,8 +36,10 @@ email.addEventListener('keyup', () => {
 
     if (emailPattern.test(emailValue)) {
         email.setAttribute('style', 'color: #58C200');
+        validemail = true;
     } else {
         email.setAttribute('style', 'color: red');
+        validemail = false;
     }
 });
 
@@ -33,8 +49,10 @@ telefone.addEventListener('keyup', () => {
 
     if (telefonePattern.test(telefoneValue)) {
         telefone.setAttribute('style', 'color: #58C200');
+        validtelefone = true;
     } else {
         telefone.setAttribute('style', 'color: red');
+        validtelefone = false;
     }
 });
 
@@ -50,22 +68,54 @@ telefone.addEventListener('keydown', (event) => {
 senha.addEventListener('keyup', () => {
     if (senha.value.length <= 5) {
         senha.setAttribute('style', 'color: red');
+        validsenha = false;
     } else {
         senha.setAttribute('style', 'color: #58C200');
+        validsenha = true;
     }
 });
 
 confirmSenha.addEventListener('keyup', () => {
     if (senha.value != confirmSenha.value) {
         confirmSenha.setAttribute('style', 'color: red');
+        validconfirmSenha = false;
     } else {
         confirmSenha.setAttribute('style', 'color: #58C200');
+        validconfirmSenha = true;
     }
 });
 //Verificações fim
 
-function cadastrar() {
+function cadastrar(){
+    if(validnome && validemail && validtelefone && validsenha && validconfirmSenha){
+        let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
 
+        listaUser.push(
+            {
+                nomeCad: nome.value,
+                emailCad: email.value,
+                telefoneCad: telefone.value,
+                senhaCad: senha.value // Corrigido aqui
+            }
+        )
+
+        localStorage.setItem('listaUser', JSON.stringify(listaUser));
+
+        msgSuccess.setAttribute('style', 'display: block');
+        msgSuccess.innerHTML = '<strong>Cadastrando usuário...</strong>';
+        msgError.setAttribute('style', 'display: none');
+        msgError.innerHTML = '';
+
+        setTimeout(()=>{
+            window.location.href = "/codigo-fonte/login/login.html"
+        }, 3000);
+        
+    } else {
+        msgError.setAttribute('style', 'display: block');
+        msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de continuar!</strong>';
+        msgSuccess.setAttribute('style', 'display: none');
+        msgSuccess.innerHTML = '';
+    }
 }
 
 btn.addEventListener('click', () => {

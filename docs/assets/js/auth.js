ef354f4d8342d0fbd53f94e16a500b305b008b06
heitorkkj <heitorsan548@gmail.com>
2024-06-03 21:56:35 -0300

    fix: routes

--- docs/assets/js/auth.js
+++ docs/assets/js/auth.js
@@ -1,7 +1,7 @@
 const logout = () => {
   localStorage.removeItem("userHasLogged");
 
-  window.location.href = "/docs/pages/login.html";
+  window.location.href = "login.html";
 };
 
 const showPassword = (type = "senha", eyeIconName = "input-password-eye") => {
@@ -36,7 +36,7 @@ const handleLogin = (e) => {
     if (findUser.password == senha) {
       localStorage.setItem("userHasLogged", true);
 
-      window.location.href = "/docs/pages/home.html";
+      window.location.href = "home.html";
     } else {
       error.setAttribute("style", "display: block");
       error.innerText = "Senha incorreta!";
@@ -93,6 +93,6 @@ const handleRegister = (e) => {
   msgError.innerHTML = "";
 
   setTimeout(() => {
-    window.location.href = "/docs/pages/home.html";
+    window.location.href = "home.html";
   }, 1500);
 };
